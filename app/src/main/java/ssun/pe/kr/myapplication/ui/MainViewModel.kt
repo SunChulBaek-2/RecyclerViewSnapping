85d117bd4fe0d565daa7eb0cee8810d418f75d50
백선철 <sunchulbaek@ncsoft.com>
2018-09-27 17:15:34 +0900

    launch 사용 수정

--- app/src/main/java/ssun/pe/kr/myapplication/ui/MainViewModel.kt
+++ app/src/main/java/ssun/pe/kr/myapplication/ui/MainViewModel.kt
@@ -4,16 +4,25 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.content.pm.ApplicationInfo
-import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.Main
 import kotlinx.coroutines.experimental.launch
 import ssun.pe.kr.myapplication.data.AppDataRepository
 import ssun.pe.kr.myapplication.data.model.Item
 import timber.log.Timber
 import javax.inject.Inject
+import kotlin.coroutines.experimental.CoroutineContext
 
 class MainViewModel @Inject constructor(
         private val repository: AppDataRepository
-): ViewModel() {
+): ViewModel(), CoroutineScope {
+
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
     private val _items = MutableLiveData<List<Item>>()
     val items: LiveData<List<Item>>
         get() = _items
@@ -22,17 +31,20 @@ class MainViewModel @Inject constructor(
     val installedApps: LiveData<List<ApplicationInfo>>
         get() = _installedApps
 
+    // TODO : 호출되지 않음
     override fun onCleared() {
         super.onCleared()
 
         Timber.d("[x1210x] onCleared()")
+
+        job.cancel()
     }
 
-    fun getItems() = launch(UI) {
+    fun getItems() = launch {
         _items.value = repository.getItems().await()
     }
 
-    fun getInstalledApps() = launch(UI) {
+    fun getInstalledApps() = launch {
         _installedApps.value = repository.getInstalledApps().await()
     }
 }
